c99db0160b77f6b48d1df26d154db5d97f7dc037
fanjianan <fanjianan@kanzhun.com>
2020-08-18 17:12:07 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/TestClass.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import java.util.Locale;
+
+/**
+ * @author fanfan
+ * @date 2020-05-23
+ **/
+public class TestClass {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.format(Locale.getDefault(), "Long value is = [%d]",755444L));
+    }
+}
